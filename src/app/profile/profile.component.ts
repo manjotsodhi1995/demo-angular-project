73855dacd2a4a67318e13fdeca686ffbc9be2f99
Manjot Singh <manjot.singh@tatadigital.com>
2021-06-28 19:18:51 +0530

    observable changes

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { FormControl, Validators } from "@angular/forms";
+import { ObjectUnsubscribedError, Observable, of, Observer } from "rxjs";
 @Component({
   selector: "app-profile",
   templateUrl: "./profile.component.html",
@@ -7,8 +8,36 @@ import { FormControl, Validators } from "@angular/forms";
 })
 export class ProfileComponent implements OnInit {
   constructor() {}
+  data = [];
+  ngOnInit() {
+    // Create a new Observable that will deliver the above sequence
+    const sequence = new Observable(this.sequenceSubscriber);
 
-  ngOnInit() {}
+    sequence.subscribe({
+      next(num) {
+        console.log(num);
+      },
+      complete() {
+        console.log("Finished sequence");
+      }
+    });
+
+    const data = of(1, 2, 3, 4);
+
+    const observer = {
+      next: x => {
+        console.log("number is ", x);
+      },
+      error: err => {
+        console.log("error is ", err);
+      },
+      complete: () => {
+        console.log("its complete");
+      }
+    };
+
+    data.subscribe(observer);
+  }
   title = "hyse academy";
   secondnumber: Number = 4;
   firstnumber: Number = 5;
@@ -22,5 +51,33 @@ export class ProfileComponent implements OnInit {
   }
   onSubmit() {
     alert("SUCCESS!! :-)\n\n" + JSON.stringify(this.model));
+  }
+
+  sequenceSubscriber(observer: Observer<number>) {
+    const seq = [1, 2, 3];
+    let timeoutId: any;
+
+    // Will run through an array of numbers, emitting one value
+    // per second until it gets to the end of the array.
+    function doInSequence(arr: number[], idx: number) {
+      timeoutId = setTimeout(() => {
+        // calling a timeout
+        observer.next(arr[idx]);
+        if (idx === arr.length - 1) {
+          observer.complete();
+        } else {
+          doInSequence(arr, ++idx);
+        }
+      }, 3000);
+    }
+
+    doInSequence(seq, 0);
+
+    // Unsubscribe should clear the timeout to stop execution
+    return {
+      unsubscribe() {
+        clearTimeout(timeoutId);
+      }
+    };
   }
 }
